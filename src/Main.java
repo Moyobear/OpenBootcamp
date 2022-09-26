5287b975f2c83da1918ce98643daa06c07ab5f05
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-09-26 10:53:45 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int res = 0;
+        res = suma(56, 23, 34);
+        System.out.println(res);
+    }
+
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
