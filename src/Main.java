1400896417a3652184d7313980a1ccfeb4c01016
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-09-26 11:38:34 -0400

    terminado

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+        //Primera parte
         int res = 0;
         res = suma(56, 23, 34);
         System.out.println(res);
+
+        //Segunda parte
+        Coche miCoche = new Coche();
+        miCoche.SumarPuertas();
+        System.out.println(miCoche.puertas);
     }
 
+    //Primera parte
     public static int suma(int a, int b, int c) {
         return a + b + c;
+    }
+}
+//Segunda parte
+class Coche {
+    public int puertas = 4;
+    public void SumarPuertas() {
+        this.puertas++;
     }
 }
